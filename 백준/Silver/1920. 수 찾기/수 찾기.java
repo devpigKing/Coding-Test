4e9dee2ffffc0d31cbbfcec15b4862e55e5a2f8b
devpigKing <137087678+devpigKing@users.noreply.github.com>
2023-08-04 22:00:57 +0900

    [Silver IV] Title: 수 찾기, Time: 2244 ms, Memory: 186372 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] ar = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            ar[i] = sc.nextInt();
+        }
+
+        Arrays.sort(ar);
+        
+        int M = sc.nextInt();
+
+
+        for ( int i = 0; i < M; i++) {
+            boolean find = false;
+            int start = 0;
+            int end = ar.length - 1;
+            int target = sc.nextInt();
+            while (start <= end) {
+                int middle = (start+end)/2;
+                int middleValues = ar[middle];
+                if(middleValues > target) {
+                    end = middle - 1;
+                } else if (middleValues < target) {
+                    start = middle + 1;
+                } else {
+                    find = true;
+                    break;
+                }
+            }
+            if (find) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+
+    }
+}
