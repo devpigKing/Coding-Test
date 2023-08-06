676fb4fbf083d95baf7d95c8532ff0efea4ce55f
devpigKing <137087678+devpigKing@users.noreply.github.com>
2023-08-06 20:41:52 +0900

    [Silver II] Title: 랜선 자르기, Time: 180 ms, Memory: 17144 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int A[] = new int[K];
+
+        long max = 0;
+
+        for ( int i = 0; i < K; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+            if (max < A[i]) {
+                max = A[i];
+            }
+        }
+
+        max++;
+
+        long start = 0;
+        long mid = 0;
+
+        while (start < max) {
+
+            mid = (start + max) / 2;
+            long cnt = 0;
+            for (int i = 0; i < K; i++) {
+                cnt += (A[i]/mid);
+            }
+            if( cnt < N ) {
+                max = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        System.out.println(start - 1);
+
+
+    }
+}
