456b34533122a1dc7858f7a503e437cbbac12f5f
devpigKing <137087678+devpigKing@users.noreply.github.com>
2023-10-25 14:58:24 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] array = new int[9];
+
+        for ( int i = 0; i < 9; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        int max = array[0];
+        int seq = 0;
+        for ( int i = 0; i < 9; i++) {
+            if ( max < array[i] ) {
+                max = array[i];
+                seq = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(seq+1);
+    }
+}
