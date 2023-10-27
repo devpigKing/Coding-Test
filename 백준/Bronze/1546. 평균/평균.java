7265629e529b43afdd757b06f77f62c6f88102e6
devpigKing <137087678+devpigKing@users.noreply.github.com>
2023-10-27 10:51:28 +0900

    [Bronze I] Title: 평균, Time: 260 ms, Memory: 18944 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -3,18 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int A[] = new int[N];
 
-        for (int i = 0; i < N; i++) {
+        int N = sc.nextInt();
+        int[] A = new int[N];
+
+        for ( int i = 0; i < N; i++) {
             A[i] = sc.nextInt();
         }
-        long sum = 0;
-        long max = 0;
-        for (int i = 0; i < N; i++) {
-            if(A[i] > max) max = A[i];
-            sum = sum + A[i];
+
+        double sum = 0;
+        double max = A[0];
+
+        for ( int ii = 0; ii < N; ii++) {
+            if ( max < A[ii] ) {
+                max = A[ii];
+            }
+            
+            sum = sum + A[ii];
         }
-        System.out.println(sum*100.0/max/N);
+        System.out.println(sum/max*100.0/N);
     }
 }
