93370fb4522e08806d9eb4e5034c4bb5043128f3
devpigKing <137087678+devpigKing@users.noreply.github.com>
2023-11-12 18:25:25 +0900

    [Gold IV] Title: 오큰수, Time: 1048 ms, Memory: 168980 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];  // 수열
+        int[] result = new int[N];  // 결과 배열
+
+        String[] str = br.readLine().split(" ");
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(str[i]);
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(0);  // 처음 스택은 항상 비어있으므로
+        for (int i = 1; i < N; i++) {
+            // 스택이 비어 있지 않고 현재 수열이 스택의 top 인덱스가 가리키는 수열보다 클 경우
+            while (!stack.isEmpty() && A[stack.peek()] < A[i]) {
+                result[stack.pop()] = A[i]; // 오큰 수
+            }
+
+            stack.push(i);
+        }
+
+        while (!stack.empty()) {
+            result[stack.pop()] = -1;
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < N; i++) {
+            bw.write(result[i] + " ");
+        }
+        bw.write("\n");
+        bw.flush();
+
+
+    }
+}
