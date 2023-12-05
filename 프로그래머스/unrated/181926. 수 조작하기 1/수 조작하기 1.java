98cc1649cc4772a41f8061d75664cfdf02bd94b2
devpigKing <137087678+devpigKing@users.noreply.github.com>
2023-12-05 19:07:16 +0900

    [level unrated] Title: 수 조작하기 1, Time: 0.99 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int n, String control) {
+        char[] controls = control.toCharArray();
+
+        for (char c : controls) {
+            switch (c) {
+                case 'w':
+                    n += 1;
+                    break;
+                case 's':
+                    n -= 1;
+                    break;
+                case 'd':
+                    n += 10;
+                    break;
+                case 'a':
+                    n -= 10;
+                    break;
+                default:
+                    n = 0;
+            }
+        }
+
+        return n;
+    }
+}
