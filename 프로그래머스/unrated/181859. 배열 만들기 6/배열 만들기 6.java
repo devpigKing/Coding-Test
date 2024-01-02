eb999cb339b6293c9aee4301eef08787aaccd596
devpigKing <137087678+devpigKing@users.noreply.github.com>
2024-01-02 17:16:41 +0900

    [level unrated] Title: 배열 만들기 6, Time: 15.79 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
@@ -2,33 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        int[] answer = cal(arr);
+        int[] answer = {};
+        Stack<Integer> stk = new Stack<>();
+
+        for(int i = 0; i < arr.length; i ++){
+            if ( stk.isEmpty()) {
+                stk.push(arr[i]);
+            }
+            else if ( stk.peek() == arr[i] ){
+                stk.pop();
+            }
+            else if ( stk.peek() != arr[i]) {
+                stk.push(arr[i]);
+            }
+        }
+
+        if ( stk.isEmpty()){
+            return new int [] {-1};
+        }
+
+        answer = new int [stk.size()];
+        for ( int i = 0; i < stk.size(); i ++){
+            answer[i] = stk.get(i);
+        }
+
         return answer;
-    }
-    private static int[] cal(int[] arr){
-        Stack<Integer> stack = new Stack<>();
-        int index = 0;
-        while(index < arr.length){
-            if(stack.isEmpty()){
-                stack.push(arr[index]);
-                index++;
-                continue;
-            }
-            if(stack.peek() == arr[index]){
-                stack.pop();
-                index++;
-            }else{
-                stack.push(arr[index]);
-                index++;
-            }
-        }
-        if(stack.size() == 0){
-            return new int[]{-1};
-        }
-        int[] result = new int[stack.size()];
-        for(int i = stack.size() - 1 ; i >= 0 ; i--){
-            result[i] = stack.pop();
-        }
-        return result;
     }
 }
