c41c8c9736c08ea091d48b326b01270a58bc501c
devpigKing <137087678+devpigKing@users.noreply.github.com>
2024-06-16 23:50:11 +0900

    Update 두 수의 합.java
    두 수의 합 - 코드리뷰

--- 프로그래머스/Lv.0/120802. 두 수의 합/두 수의 합.java
+++ 프로그래머스/Lv.0/120802. 두 수의 합/두 수의 합.java
@@ -1,7 +1,7 @@
 class Solution {
-    public int solution(int num1, int num2) {
-        int answer = -1;
-        answer = num1 + num2;
-        return answer;
+    public int solution(int num1, int num2) { // solution 함수는 인자로 int형 변수 num1과 num2를 받는다.
+        int answer = -1; // int 라는 변수를 선언 후 초기화
+        answer = num1 + num2; // answer 변수에 입력받은 num1과 num2의 합을 할당한다.
+        return answer; // answer를 solution 함수의 결괏값으로 반환
     }
 }
