966a3c6382e6c7203d313ba7500dd4b5a7efbd71
devpigKing <137087678+devpigKing@users.noreply.github.com>
2024-06-17 00:05:18 +0900

    [level 0] Title: 몫 구하기, Time: 0.01 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int num1, int num2) { // solution이라는 함수에 int형 변수 num1, num2를 입력받음.
+        int answer = 0; // 결괏값을 담아둘 answer 변수를 선언 후 초기화 
+        answer = num1 / num2; // num1 / num2의 값을 answer에 할당
+        return answer; // solution 함수의 결괏값으로 answer 반환
+    }
+}
