047ffc05e1c402e621190506a8d58c23d2d080c7
devpigKing <137087678+devpigKing@users.noreply.github.com>
2024-06-18 23:53:25 +0900

    [level 0] Title: 나머지 구하기, Time: 0.01 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int solution(int num1, int num2) { // 매개변수로 int형 변수 num1, num2를 받는다. 
+        int answer = num1%num2; // num1을 num2로 나눈 나머지를 int형 변수 answer에 삽입.
+        return answer; // answer 반환
+    }
+}
