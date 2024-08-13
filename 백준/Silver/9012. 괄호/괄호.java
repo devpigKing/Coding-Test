7f9371c9d6915108e0972fc1d11765bc20c21fef
devpigKing <137087678+devpigKing@users.noreply.github.com>
2024-08-13 18:22:28 +0900

    [Silver IV] Title: 괄호, Time: 232 ms, Memory: 17864 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -5,34 +5,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int T = sc.nextInt();
+        int T = sc.nextInt(); // 입력 받을 문자열의 개수 입력 
         
-        for (int i = 0; i < T; i++) {
-            System.out.println(stack(sc.next()));
+        for (int i = 0; i < T; i++) { // 0부터 T-1번까지 T번 돌면서
+            System.out.println(stack(sc.next())); // stack 메서드에 하나씩 보낸다!
         }
     }
     
     public static String stack(String s) {
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>(); // 스택 생성
         
-        for (char c : s.toCharArray()) {
+        for (char c : s.toCharArray()) { // s 문자열을 배열로 바꿔서 문자 하나하나 돌면서 비교!
             switch (c) {
-                case '(':
-                    stack.push(c);
-                    break;
-                case ')':
-                    if (stack.isEmpty()) {
-                        return "NO";
+                case '(': // 열린 괄호이면
+                    stack.push(c); // 스택에 푸시
+                    break; 
+                case ')': // 닫힌 괄호라면
+                    if (stack.isEmpty()) { // 스택이 비어있다면 짝을 이룰 수 없으므로
+                        return "NO"; // NO 출력
                     }
-                    stack.pop();
+                    stack.pop(); // stack에 뭔가 있다면 열린 괄호이므로 pop해서 짝 지어줌.
                     break;
             }
         }
         
-        if (stack.isEmpty()) {
-            return "YES";
-        } else {
-            return "NO";
+        if (stack.isEmpty()) { // 반복문이 끝나고 스택이 비어있다면
+            return "YES"; // 조건 만족
+        } else { // 뭔가 남아있다면(열린 괄호)
+            return "NO"; // 조건 불만족
         }
     }
 }
