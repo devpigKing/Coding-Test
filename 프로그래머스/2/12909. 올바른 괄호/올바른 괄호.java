54fdf54666edf588a850ee0c3dd74331ce0392e0
devpigKing <137087678+devpigKing@users.noreply.github.com>
2024-08-13 17:38:19 +0900

    [level 2] Title: 올바른 괄호, Time: 17.14 ms, Memory: 53.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack();
+        
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                    case '(' -> stack.push(c);
+                    case ')' -> {
+                        if (stack.isEmpty()) return false;
+                        stack.pop();
+                    }
+            }
+        }
+        
+        return stack.isEmpty();
+
+    }
+}
