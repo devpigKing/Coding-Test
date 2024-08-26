965f45968a2bc412e7df8431e511c3b43512c4df
devpigKing <137087678+devpigKing@users.noreply.github.com>
2024-08-27 00:25:24 +0900

    [D1] Title: 평균값 구하기, Time: 161 ms, Memory: 20,956 KB -BaekjoonHub

--- SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
@@ -1,20 +1,23 @@
-import java.util.Scanner;
+import java.util.*;
 
-class Solution {
-		public static void main(String[] args) {
-				Scanner sc = new Scanner(System.in);
-				int T = sc.nextInt();
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
 
-				for ( int tc = 1; tc <= T; tc++) {
-						double sum = 0;
-						
-						for (int i = 0; i<10; i++) {
-								int num = sc.nextInt();
-								sum += num;
-						}
-						double avg = Math.round(sum/10);
-						System.out.printf("#%d %d\n", tc, (int)avg);
-				}
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
 
-		}
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			double num = 0;
+            
+            for (int i = 0; i < 10; i++) {
+                num += sc.nextInt();
+			}
+            System.out.println("#" + test_case + " " + Math.round(num/10));
+        }
+	}
 }
