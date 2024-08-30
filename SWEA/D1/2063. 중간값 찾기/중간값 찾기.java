e2e3a22eb3d16716c86961e36460198e57623024
devpigKing <137087678+devpigKing@users.noreply.github.com>
2024-08-30 20:15:29 +0900

    [D1] Title: 중간값 찾기, Time: 138 ms, Memory: 20,952 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -1,19 +1,21 @@
-import java.util.Scanner;
-import java.util.Arrays;
 
-class Solution {
-		public static void main(String[] args) {
-				Scanner sc = new Scanner(System.in);
-				int N = sc.nextInt();
-				int[] A = new int[N];
+import java.util.*;
+import java.io.FileInputStream;
 
-				for ( int i = 0; i < N; i++) {
-						A[i] = sc.nextInt();
-				} 
-				Arrays.sort(A);
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
 
-				int mid = N/2;
-				System.out.println(A[mid]);
-
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int mid = N/2;
+        int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+		    arr[i] = sc.nextInt();
 		}
+		Arrays.sort(arr);
+		
+		System.out.println(arr[mid]);
+	}
 }
