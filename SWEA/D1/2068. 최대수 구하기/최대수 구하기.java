f810b79601475dda09e89bf9d76b5f59ed0bbff7
devpigKing <137087678+devpigKing@users.noreply.github.com>
2024-08-30 19:47:59 +0900

    [D1] Title: 최대수 구하기, Time: 125 ms, Memory: 20,564 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -1,19 +1,30 @@
+
 import java.util.Scanner;
+import java.io.FileInputStream;
 
-class Solution {
-		public static void main(String[] args) {
-				Scanner sc = new Scanner(System.in);
-				int T = sc.nextInt();
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
 
-				for (int tc = 1; tc <= T; tc++) {
-						int max = 0;
-						for ( int i = 0; i < 10; i++) {
-								int num = sc.nextInt();
-								if ( max < num ) {
-										max = num;
-								}
-						}
-						System.out.println("#" + tc + " " + max);
-				}
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		
+			int max = 0;
+		    
+		    for (int i = 0; i < 10; i++) {
+				    int num = sc.nextInt();
+				    
+				    if (max < num) {
+						    max = num;
+				    } 
+		    }
+			System.out.println("#" + test_case + " " + max);
+
 		}
+	}
 }
