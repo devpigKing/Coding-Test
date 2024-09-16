a79abc47678b1f693f2995bbeeac2e3207a89403
devpigKing <137087678+devpigKing@users.noreply.github.com>
2024-09-16 22:09:48 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 164 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
 		static int answer = 0;
     public static void main(String args[]) {
-		    Scanner sc = new Scanner(System.in);
+		    Scanner sc = new Scanner(System.in); // 입력값이 많지 않아서 Scanner
 		    String exam = sc.nextLine();
-		    String[] str = exam.split("-"); // -기준으로 나눠서 배열에
+		    String[] str = exam.split("-"); // - 기준으로 나눠서 배열에 삽입
 		    
-		    for (int i=0; i < str.length; i++) {
-				    int temp = mySum(str[i]);
+		    for (int i=0; i < str.length; i++) { // str을 돌며 
+				    int temp = mySum(str[i]); 
 				    if (i == 0) answer = answer + temp;
 				    else answer = answer - temp;
 		    }
@@ -16,15 +16,15 @@ public class Main {
 		    System.out.println(answer);
     }
     
-    private static int mySum(String a) {
-		    int sum = 0;
-		    String[] temp = a.split("[+]");
-		    
+    private static int mySum(String a) { // 괄호 안 숫자들의 합을 더하는 메서드
+		    int sum = 0; 
+		    String[] temp = a.split("[+]"); // +를 기준으로 split
+		    // +는 시스템에서 잘 안읽히는 경우가 많아서 양 옆에 []괄호
 		    for(int i = 0; i < temp.length; i++) {
-				    sum += Integer.parseInt(temp[i]);
+				    sum += Integer.parseInt(temp[i]); 
 		    }
 		    
-		    return sum;
+		    return sum; // 다음 -가 나오기 전까지의 괄호안 숫자들의 합
     }
     
 }
