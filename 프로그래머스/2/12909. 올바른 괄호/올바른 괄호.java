2ad62965ed0225f479655c1761f03c578a918c6b
devpigKing <137087678+devpigKing@users.noreply.github.com>
2024-10-12 21:38:31 +0900

    [level 2] Title: 올바른 괄호, Time: 15.93 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,20 +1,18 @@
-import java.util.*;
+import java.util.ArrayDeque;
 
 class Solution {
-    boolean solution(String s) {
-        Stack<Character> stack = new Stack();
-        
-        for (char c : s.toCharArray()) {
-            switch (c) {
-                    case '(' -> stack.push(c);
-                    case ')' -> {
-                        if (stack.isEmpty()) return false;
-                        stack.pop();
-                    }
-            }
-        }
-        
-        return stack.isEmpty();
-
-    }
+		public boolean solution(String s) {
+				ArrayDeque<Character> stack = new ArrayDeque<>(); // 스택 생성
+				
+				char[] a = s.toCharArray(); // 문자열을 
+				for (char c : a) { // 문자열 a를 전부 돌면서
+						if (c == '(') { // 문자열 c가 여는 괄호인 경우
+								stack.push(c); // 스택에 여는 괄호 추가
+						} else { // 여는 괄호가 아닌 경우 
+								if (stack.isEmpty() || stack.pop() == c) // 스택이 비어있거나, pop한 값이 열린 괄호가 아닌 경우
+										return false;
+						}
+				}
+				return stack.isEmpty();
+		}
 }
