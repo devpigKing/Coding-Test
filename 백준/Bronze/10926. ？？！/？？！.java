3d5bbfe24ce781fc18d43ce26936204f7e5d61e0
devpigKing <137087678+devpigKing@users.noreply.github.com>
2025-04-29 08:02:30 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         String lastString = "??!";
         
-        String s = sc.next();
-        
+        String s = br.readLine();
         System.out.println(s + lastString);
-
     }
-}
     
+}
